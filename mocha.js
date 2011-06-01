2b4affb0d8c6a21623d2051c053a1d4b649f2fab
Matt Campbell <woogley@gmail.com>
2011-05-31 20:55:32 -0400

    Adding canvas prototype

--- mocha.js
+++ mocha.js
@@ -1,12 +1,42 @@
-(function () {
-	var isBrowser = typeof window !== 'undefined' && this === window;
-	this.print = print;
+/* == Mocha ==
+* Simple game engine that runs in a browser or Rhino
+* @author Matt Campbell <woogley@gmail.com>
+*/
 
-	if (isBrowser) {
-		this.print = function () {
-			console.log.apply(console, arguments);
-		};
+(function () {
+	var isBrowser, global, Mocha, document;
+	isBrowser = typeof window !== 'undefined' && this === window;
+	global = this;
+	document = global.document;
+
+	if (!isBrowser) {
+		importPackage(java.awt);
+		importPackage(java.awt.image);
+		importPackage(javax.swing);
 	}
 
-	this.print(isBrowser);
+	Mocha = global.Mocha = function (game, width, height) {
+		this.game = game;
+		this.canvas = new Mocha.Canvas(this, width, height);
+	};
+
+	Mocha.Canvas = (function () {
+		return isBrowser && function (context, width, height) {
+			this.canvas = document.createElement('canvas');
+			this.canvas.width = width;
+			this.canvas.height = height;
+			this.brush = this.canvas.getContext('2d');
+			document.body.appendChild(this.canvas);
+		} || function (context, width, height) {
+			this.frame = new JFrame();
+			this.frame.setResizable(false);
+			this.frame.setTitle(context.game.title || 'Game');
+			this.canvas = new JPanel();
+			this.canvas.setPreferredSize(new Dimension(width, height)); 
+			this.frame.add(this.canvas);
+			this.frame.pack();
+			this.frame.setLocationRelativeTo(null);
+			this.frame.show();
+		};
+	}());
 }());
