a1fc29a0c91c099d33d5e967f2155e68612c9ca4
Matt Campbell <woogley@gmail.com>
2011-05-31 21:27:23 -0400

    Adding update/render loop

--- mocha.js
+++ mocha.js
@@ -36,7 +36,82 @@
 			this.frame.add(this.canvas);
 			this.frame.pack();
 			this.frame.setLocationRelativeTo(null);
-			this.frame.show();
+			this.frame.setDefaultCloseOperation(this.frame.EXIT_ON_CLOSE);
 		};
+	}());
+
+	Mocha.Canvas.prototype.getBrush = (function () {
+		return isBrowser && function () {
+			return this.brush;
+		} || function () {
+			return this.brush !== null && this.brush || this.frame.getGraphics();
+		};
+	}());
+
+	Mocha.Canvas.prototype.setColor = (function () {
+		return isBrowser && function (r, g, b) {
+			this.brush.fillStyle = this.brush.strokeStyle = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+		} || function (r, g, b) {
+			if (this.getBrush()) {
+				this.brush.setColor(new Color(r, g, b));
+			}
+		};
+	}());
+
+	Mocha.Canvas.prototype.fillRect = (function () {
+		return isBrowser && function (x, y, w, h) {
+			this.brush.fillRect(x, y, w, h);
+		} || function (x, y, w, h) {
+			if (this.getBrush()) {
+				this.brush.fillRect(x, y, w, h);
+			}
+		};
+	}());
+
+	Mocha.prototype.start = function () {
+		this.game.init(this);
+		this.game.start(this);
+
+		this.loop();
+	};
+
+	Mocha.prototype.loop = (function () {
+		return isBrowser && function () {
+			var loop, lastUpdate, now, delta;
+			loop = window.webkitRequestAnimationFrame ||
+				window.mozRequestAnimationFrame ||
+				window.oRequestAnimationFrame ||
+				function (callback) {
+					window.setTimeout(function () {
+						callback(Date.now());
+					}, 20);
+				};
+			
+			lastUpdate = Date.now();
+			(function update(time) {
+				now = Date.now();
+				delta = now - lastUpdate;
+				game.update(delta);
+				game.render(this.canvas);
+				lastUpdate = now + 0;
+
+				loop(update);
+			}(Date.now()));
+
+		} || function () {
+			spawn(function () {
+				this.canvas.frame.show();
+				var now, delta, lastUpdate = Date.now();
+
+				while (true) {
+					now = Date.now();
+					delta = now - lastUpdate;
+					game.update(delta);
+					game.render(this.canvas);
+					lastUpdate = now + 0;
+					java.lang.Thread.sleep(10);
+				}
+			}.bind(this));
+		}
 	}());
 }());
